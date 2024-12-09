a820690bbf77d9314ea4c5acc8e5d3d45b5f755f
Tomorrow619 <abror619@gmail.com>
2024-12-09 21:31:02 +0500

    git

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function activeArea(){
 const field = document.getElementsByClassName("field");
 //["field","field",.....]
 gameBtn.innerHTML = "ЗАВЕРШИТЬ ИГРУ"
+gameBtn.style.backgroundColor = "red"
 for (let i = 0;i< field.length; i++){
     setInterval(()=> {
         field[i].classList.add("active");
