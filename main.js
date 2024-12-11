f3eb4d06a0b887e1441409e8013303cb46fc327f
Tomorrow619 <abror619@gmail.com>
2024-12-11 21:44:48 +0500

    регистрацию и логин игры сапер

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
 //Кнопки
 const gameBtn = document.getElementById("gameBtn");
+
 //Слушатели событий
 gameBtn.addEventListener("click", activeArea);
-
+document.querySelector("#loginWrapper form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+    auth()
+})
 function activeArea(){
 const field = document.getElementsByClassName("field");
 //["field","field",.....]
@@ -15,4 +19,93 @@ for (let i = 0;i< field.length; i++){
     }, 20 * i)
 }
 
+}
+
+let USERNAME;
+
+
+async function auth(){
+    const loginWrapper= document.getElementById("loginWrapper")
+    const login = document.getElementById("login").value
+    let response = await sendRequest("user", "GET",{
+        username:login,
+    });
+    if(response.error){
+        //ПОльзователь не зарегался
+        let registration = await sendRequest("user","POST",{ username: login });
+        if (registration.error) {
+            alert(registration.message);
+          } else {
+            USERNAME = login;
+            loginWrapper.style.display = "none";
+            updateUSerBalance();
+          }
+      
+        
+
+    }
+    else {
+        USERNAME = login;
+        loginWrapper.style.display="none";
+        updateUSerBalance()
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+async function updateUSerBalance(){
+    let response= await sendRequest ("user","GET",{
+        username: USERNAME 
+    })
+if(response.error){
+    //Если есть ошибка
+    alert(response.message)
+} else {
+    const user = document.querySelector("header span" )
+    user.innerHTML = `Пользователь ${response.username} c балансом ${response.balance}`
+
+}
+console.log(response);
+}
+
+
+
+updateUSerBalance()
+
+
+async function sendRequest(url, method, data) {
+    url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`
+    
+    if(method == "POST") {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    
+        response = await response.json()
+        return response
+    } else if(method == "GET") {
+        url = url+"?"+ new URLSearchParams(data)
+        let response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        response = await response.json()
+        return response
+    }
 }
